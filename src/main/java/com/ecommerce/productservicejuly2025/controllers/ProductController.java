cc6f73fb405ddf2d94c9998c192fd51019708f8a
Kariz Khan <khankarix999@gmail.com>
2025-08-07 01:48:27 +0530

    today I implemented product repository and hit getsingle product api through dpproduct service

--- src/main/java/com/ecommerce/productservicejuly2025/controllers/ProductController.java
+++ src/main/java/com/ecommerce/productservicejuly2025/controllers/ProductController.java
@@ -4,6 +4,7 @@ package com.ecommerce.productservicejuly2025.controllers;
 import com.ecommerce.productservicejuly2025.exceptions.ProductNotFoundException;
 import com.ecommerce.productservicejuly2025.models.Product;
 import com.ecommerce.productservicejuly2025.services.ProductService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class ProductController {
 
     ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(@Qualifier("dbProductService") ProductService productService) {
         this.productService = productService;
     }
 
--- src/main/java/com/ecommerce/productservicejuly2025/services/DbProductService.java
+++ src/main/java/com/ecommerce/productservicejuly2025/services/DbProductService.java
@@ -2,13 +2,31 @@ package com.ecommerce.productservicejuly2025.services;
 
 import com.ecommerce.productservicejuly2025.exceptions.ProductNotFoundException;
 import com.ecommerce.productservicejuly2025.models.Product;
+import com.ecommerce.productservicejuly2025.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service("dbProductService")
 public class DbProductService implements ProductService {
+
+    ProductRepository productRepository;
+
+    public DbProductService(ProductRepository productReposiitory){
+        this.productRepository = productReposiitory;
+    }
+
+
     @Override
     public Product getSingleProduct(Long ProductId) throws ProductNotFoundException {
-        return null;
+       Optional<Product> optionalProduct = productRepository.findById(ProductId); // optional product because product cud be null and when gettitle will be used itll throw not found exception
+
+        if(optionalProduct.isEmpty()){
+            throw new ProductNotFoundException("Product with id "+ ProductId + "does not exist"  );
+        }
+
+        return optionalProduct.get();
     }
 
     @Override
